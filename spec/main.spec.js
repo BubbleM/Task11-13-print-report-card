60ec763de36a8fe78f04cbc462dea3eabc32548f
BubbleM <1870408473@qq.com>
2017-08-23 23:50:10 +0800

    add unit test print

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -2,7 +2,7 @@ let main = require('../lib/main');
 let expect = require('chai').expect;
 
 describe('测试描述', function () {
-    it('1+1', function () {
-        expect(main(1,2)).to.be.equal(3);
-    });
+  it('1+1', function () {
+    expect(main(1, 2)).to.be.equal(3);
+  });
 });
