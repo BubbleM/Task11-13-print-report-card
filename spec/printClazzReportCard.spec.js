a68f98a6867b67599f4dc9b3697b0727b4aba878
BubbleM <1870408473@qq.com>
2017-08-25 10:32:40 +0800

    update print all the student scores

--- spec/printClazzReportCard.spec.js
+++ spec/printClazzReportCard.spec.js
@@ -1,6 +1,6 @@
 let expect = require('chai').expect;
 let Clazz = require('../lib/Clazz');
-let printReportCard = require('../lib/printReportCard');
+let printClazzReportCard = require('../lib/printClazzReportCard');
 let addStudent = require('../lib/addStudent');
 
 let classes = [];
@@ -22,7 +22,7 @@ function addClasses(clazzId, student) {
 
 describe('测试描述', function () {
   const str1 = 'Bubble,140102,1401,Math:21,Chinese:70,English:80,Program:90';
-  const str2 = 'Nichole,140203,1402,English:90,Math:23,Chinese:90,Program:29';
+  const str2 = 'Nichole,140103,1401,English:90,Math:23,Chinese:90,Program:29';
   const str3 = 'liumin,140104,1401,English:20,Math:39,Chinese:56,Program:38';
   let student1 = addStudent(str1);
   let student2 = addStudent(str2);
@@ -34,17 +34,18 @@ describe('测试描述', function () {
     addClasses(student3.clazz, student3);
   });
 
-  it('print reportCard while student is not in the same class', function () {
-    let result = printReportCard('140102,140203', classes);
+  it('print reportCard while student in the same class', function () {
+    let result = printClazzReportCard('140102', classes);
 
-    expect(result).to.be.equal(`成绩单
+    expect(result).to.be.equal(`学号为140102学生所在1401班级成绩单
 姓名|数学|语文|英语|编程|平均分|总分 
 ========================
 Bubble|21|70|80|90|65.25|261
 Nichole|23|90|90|29|58|232
+liumin|39|56|20|38|38.25|153
 ========================
-全校总分平均数：215
-全校总分中位数：232`);
+全班总分平均数：215
+全班总分中位数：232`);
   });
 
 });
